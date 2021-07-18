837a7a1f26cf2157517ba9566f9d2746f68b0bc6
Benjamin Rose <benthere914@gmail.com>
2021-07-18 16:43:02 -0500

    recursive .includes function added

---
+++ recursive.js
@@ -0,0 +1,17 @@
+function recursiveIncludes(subString, string, subStringArray = subString.split(""), stringArray = string.split("")) {
+    //base case 1
+    if ((subStringArray.length === stringArray.length) && (subString !== string)) return false;
+    //base case 2
+    if (subString.length > string.length) return false;
+    //base case 3
+    if (subStringArray.reduce((bool, ele, i) => { if (ele !== stringArray[i]) { bool = false } return bool; }, true)) return true;
+    //recursive case
+    else {
+        //recursive step
+        stringArray.shift();
+        //recursive call
+        return recursiveIncludes(subString, string, subStringArray, stringArray);
+    }
+}
+// console.log(recursiveIncludes("hello", "hllo world"))
+
