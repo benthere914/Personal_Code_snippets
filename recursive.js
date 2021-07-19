6f219aab46e3ffc5463fec04985aa8842556880d
Benjamin Rose <benthere914@gmail.com>
2021-07-18 19:58:40 -0500

    clean up code / add pseudo code

--- recursive.js
+++ recursive.js
@@ -34,11 +34,11 @@ function fibonacci(num, res = [0, 1], counter = 2){
     //recursive case
     else {
         //adds the next element to res
-        res.push(res[counter - 1] + res[counter - 2])
+        res.push(res[counter - 1] + res[counter - 2]);
         //recursive step
-        counter++
+        counter++;
         //recursive call
-        return fibonacci(num, res, counter)
+        return fibonacci(num, res, counter);
     }
 }
 // showFibonacci = (start, end = start + 1) => {for (let i = start; i <= end; i++){console.log(fibonacci(i))}};
@@ -48,17 +48,21 @@ function sumOfArray(array){
     //base case
     if (!array.length) return 0;
     //recursive step and recursive call
-    return array.pop() + sumOfArray(array)
+    return array.pop() + sumOfArray(array);
 }
 
 // console.log(sumOfArray([0,1,2,3,4,5,6,7,8,9,10])) // 55 
 
 function findSmallestInArray(array, tempArray = [...array],smallest = tempArray[tempArray.length - 1], index = tempArray.length, counter = tempArray.length){
+    //base case
     if (!tempArray.length) return [smallest, index];
+    //recursive case
     else {
         counter--;
+        //recursive step
         let last = tempArray.pop();
         if (last <= smallest) {smallest = last; index = counter};
+        //recursive call
         return findSmallestInArray(array, tempArray, smallest, index, counter);
     }
 }
@@ -67,11 +71,15 @@ function findSmallestInArray(array, tempArray = [...array],smallest = tempArray[
 
 
 function findLargestInArray(array, tempArray = [...array],largest = tempArray[tempArray.length - 1], index = tempArray.length, counter = tempArray.length){
+    //base case
     if (!tempArray.length) return [largest, index];
+    //recursive case
     else {
         counter--;
+        //recursive step
         let last = tempArray.pop();
         if (last >= largest) {largest = last; index = counter};
+        //recursive call
         return findLargestInArray(array, tempArray, largest, index, counter);
     }
 }
@@ -79,15 +87,15 @@ function findLargestInArray(array, tempArray = [...array],largest = tempArray[te
 // console.log(findLargestInArray([45, 100, 10]))
 
 function increasingSort(array, tempArray = [...array], res = []) {
-    debugger;
+    //base case
     if (!tempArray.length){debugger; return res;}
+    //recursive case
     else {
         let [smallest, index] = [...findSmallestInArray(tempArray)];
-        debugger
         res.push(smallest);
-        debugger;
+        //recursive step
         tempArray.splice(index, 1);
-        debugger;
+        //recursive call
         return increasingSort(array, tempArray, res);
     }
 }
@@ -96,15 +104,15 @@ function increasingSort(array, tempArray = [...array], res = []) {
 
 
 function decreasingSort(array, tempArray = [...array], res = []) {
-    debugger;
+    //base case
     if (!tempArray.length){debugger; return res;}
+    //recursive case
     else {
         let [largest, index] = [...findLargestInArray(tempArray)];
-        debugger
         res.push(largest);
-        debugger;
+        // recursive step
         tempArray.splice(index, 1);
-        debugger;
+        // recursive call
         return decreasingSort(array, tempArray, res);
     }
 }
