a3ac1cf12049b27aa3f0f2b4ce9334ad4a83044e
Benjamin Rose <benthere914@gmail.com>
2021-07-27 20:44:20 -0500

    made a binary to hex function

--- binaryHexDecConversions.js
+++ binaryHexDecConversions.js
@@ -38,3 +38,45 @@ function hexToDec(input){
     return null;
 }
 
+
+function binaryToHex(input){
+    if(!input){return null}
+    input = input.toLowerCase()
+    switch (input){
+        case "0000":
+            return 0;
+        case "0001": 
+            return 1;
+        case "0010": 
+            return 2;
+        case "0011":
+            return 3;
+        case "0100":
+            return 4;
+        case "0101":
+            return 5;
+        case "0110":
+            return 6;
+        case "0111": 
+            return 7;
+        case "1000": 
+            return 8;
+        case "1001":
+            return 9;
+        case "1010":
+            return 10;
+        case "1011": 
+            return 11;
+        case "1100": 
+            return 12;
+        case "1101":
+            return 13;
+        case "1110":
+            return 14;
+        case "1111":
+            return 15;
+    }
+    return null;
+}
+
+console.log(binaryToHex("0100"))
