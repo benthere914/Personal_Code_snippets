62c8f324b4a487816c5ba61a948d19ada02c8845
Benjamin Rose <benthere914@gmail.com>
2021-07-27 22:37:30 -0500

    added loop that shows all functions that need to be made

--- binaryHexDecConversions.js
+++ binaryHexDecConversions.js
@@ -117,4 +117,11 @@ function binaryToDec(binary){
     }
     return output;
 }
-
+let arr = ["hex", "dec", "binary", "ascii"]
+for (let i = 0; i < arr.length; i++){
+    for(let j = 0; j < arr.length; j++){
+        if (i !== j){
+            console.log(arr[i], arr[j])
+        }
+    }
+}
